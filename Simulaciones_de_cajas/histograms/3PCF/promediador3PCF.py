c6ee2574f0fe3098ac7e6579e1a3ae9022f0d851
AlejandroGoper <ia.gomezperez@ugto.mx>
2021-09-28 21:44:49 -0500

    Realizo mapeo a nuevas variables: hay problemas

--- Simulaciones_de_cajas/histograms/3PCF/promediador3PCF.py
+++ Simulaciones_de_cajas/histograms/3PCF/promediador3PCF.py
@@ -25,6 +25,8 @@ ddr,e_ddr = get_histogram("DDR", "512MPc", 15)
 drr,e_drr = get_histogram("DRR", "512MPc", 15)
 rrr,e_rrr = get_histogram("RRR", "512MPc", 15)
 
+
+"""
 # Definiendo eje de referencia de la distancia r
 
 bin_index = np.arange(30)
@@ -41,7 +43,7 @@ r3 = r
 _3PCF = PCF(_DDD=ddd, _DDR=ddr, _DRR=drr, _RRR = rrr)
 estimador_ss = _3PCF.estimar_3PCF()
 error = _3PCF.error_estimador_ss(e_DDD=e_ddd, e_DDR=e_ddr, e_DRR=e_drr, e_RRR = e_rrr)
-
+"""
 
 """
 
@@ -53,7 +55,7 @@ Graficando cortes de la 3PCF:
     - Graficamos esta matriz con imshow de python si solo no quiero mapear los ejes, y con pcolormesh si quiero mapear los ejes.
     
 
-"""
+
 
 # Graficando histograma 
 class MidpointNormalize(Normalize):
@@ -89,10 +91,10 @@ R1, R2 = np.meshgrid(r_,r_)
 
 # Opcion grafica 3
 
-"""
-Vamos a aplanar todo el array de la 3PCF para poderlo graficar en 2D
 
-"""
+#Vamos a aplanar todo el array de la 3PCF para poderlo graficar en 2D
+
+
 aplanado_3PCF = estimador_ss.flatten()
 
 
@@ -113,6 +115,53 @@ plt.ylabel(r"$\epsilon$",fontsize=22)
 plt.yticks(fontsize=15)
 plt.savefig("treeCorr_3PCF_512MPc_0.png")
 plt.show()
+
+"""
+
+"""
+Mapeo a las variables de TreeCorr
+
+r = d2
+u = d3 / d2
+v = (d1 - d2)/d3
+
+Vamos a mapear los histogramas directamente.
+"""
+
+# Construimos unas histogramas vacios para las nuevas variables, r,u,v
+
+ddd_mapeo = np.zeros((30,30,30))
+rrr_mapeo = np.zeros((30,30,30))
+drr_mapeo = np.zeros((30,30,30))
+rdd_mapeo = np.zeros((30,30,30))
+
+# Recorremos el histograma clasico respetando la jerarquia de treccor d1>d2>d3
+
+c = 140/30
+# Esto es porque para las variables u,v el valor maximo es 1, y nbins = 30
+c_u = 1/30
+c_v = c_u
+
+for i in range(29):
+    for j in range(i):
+        for k in range(j):
+            # Guardamos la contribucion del bin pivote en nuestro histograma
+            ddd_value = ddd[i][j][k]
+            rrr_value = rrr[i][j][k]
+            drr_value = drr[i][j][k]
+            rdd_value = ddr[i][j][k]
+            print(f"i={i}, j={j}, k={k}")
+            # Calculamos las nuevas variables
+            r = c*(i+1)
+            u = (k+1)/(j+1)
+            v = (i-j)/(k+1)
+            print(f"r={r}, u={u}, v={v}")
+            # Calculamos ahora el indice que le corresponde en el array de nuevas variables
+            i_r = int(r/c)
+            i_u = int(u/c_u)
+            i_v = int(v/c_v)
+            print(f"i_r = {i_r}, i_u= {i_u}, i_v= {i_v}")
+            
 
 
 
