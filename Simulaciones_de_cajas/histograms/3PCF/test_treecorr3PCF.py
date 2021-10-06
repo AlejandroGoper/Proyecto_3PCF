ef4e94443dc170d2039276e8ef7282b330587542
AlejandroGoper <ia.gomezperez@ugto.mx>
2021-10-05 23:23:36 -0500

    Cambios para prueba en 1K

--- Simulaciones_de_cajas/histograms/3PCF/test_treecorr3PCF.py
+++ Simulaciones_de_cajas/histograms/3PCF/test_treecorr3PCF.py
@@ -13,17 +13,17 @@ python y generar los histogramas de la 3PCF isotropica.
 
 
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import treecorr as tc
-from matplotlib.colors import Normalize
-from matplotlib.cm import RdBu
-from modulo_preparacion_tc import eliminar_v_negativos
+#from matplotlib.colors import Normalize
+#from matplotlib.cm import RdBu
+#from modulo_preparacion_tc import eliminar_v_negativos
 # Importando los archivos de data y random
 
 path = "../../"
 
-data = np.genfromtxt(path+"data/data_512MPc_0.dat")
-random = np.genfromtxt(path+"random/rand0_512MPc_0.dat")
+data = np.genfromtxt(path+"data/data_1K.dat")
+random = np.genfromtxt(path+"random/rand0_1K.dat")
 
 # Separamos las x, y, z y w en arreglos numpy para procesarlos con treecorr
 
@@ -46,23 +46,30 @@ w_r = random[:,3]
 
 # Catalogamos estos puntos con una clase de treecorr
 
-d_cat = tc.Catalog(x=x_d[:1000], y=y_d[:1000], z=z_d[:1000] ,w=w_d[:1000])
-r_cat = tc.Catalog(x=x_r[:1000], y=y_r[:1000], z=z_r[:1000], w=w_r[:1000])
+d_cat = tc.Catalog(x=x_d, y=y_d, z=z_d ,w=w_d)
+r_cat = tc.Catalog(x=x_r, y=y_r, z=z_r, w=w_r)
 
 
 # Procesamos los datos
 
-ddd = tc.NNNCorrelation(nbins= 30, bin_type= "LogRUV", metric= "Euclidean", min_sep= 1, max_sep=140, bin_slop= 0, nubins=30,nvbins=30)
-rdd = tc.NNNCorrelation(nbins= 30, bin_type= "LogRUV", metric= "Euclidean", min_sep= 1, max_sep=140, bin_slop= 0, nubins=30,nvbins=30)
-drr = tc.NNNCorrelation(nbins= 30, bin_type= "LogRUV", metric= "Euclidean", min_sep= 1, max_sep=140, bin_slop= 0, nubins=30,nvbins=30)
-rrr = tc.NNNCorrelation(nbins= 30, bin_type= "LogRUV", metric= "Euclidean", min_sep= 1, max_sep=140, bin_slop= 0, nubins=30,nvbins=30)
-
+ddd = tc.NNNCorrelation(nbins= 120, bin_type= "LogRUV", metric= "Euclidean", min_sep= 1, max_sep=140, bin_slop= 0, nubins=120,nvbins=120)
+rdd = tc.NNNCorrelation(nbins= 120, bin_type= "LogRUV", metric= "Euclidean", min_sep= 1, max_sep=140, bin_slop= 0, nubins=120,nvbins=120)
+drr = tc.NNNCorrelation(nbins= 120, bin_type= "LogRUV", metric= "Euclidean", min_sep= 1, max_sep=140, bin_slop= 0, nubins=120,nvbins=120)
+rrr = tc.NNNCorrelation(nbins= 120, bin_type= "LogRUV", metric= "Euclidean", min_sep= 1, max_sep=140, bin_slop= 0, nubins=120,nvbins=120)
 ddd.process(d_cat)
 rrr.process(r_cat)
 drr.process(d_cat,r_cat)
 rdd.process(r_cat,d_cat)
 
-ddd.write('output_TC_1K_30b.dat',rrr,drr,rdd)
+ddd.get_histograms()
+
+#ddd.write('output_TC_1K_120b.dat',rrr,drr,rdd)
+
+
+
+
+
+"""
 # Calculamos el estimador zeta 
 
 zeta, var_zeta = ddd.calculateZeta(rrr=rrr,drr=drr,rdd=rdd)
@@ -86,7 +93,7 @@ class MidpointNormalize(Normalize):
 
 norm = MidpointNormalize(midpoint=0)
 
-xi = eliminar_v_negativos("output_TC_1K_30b.dat", nbins=30)
+xi = eliminar_v_negativos("output_TC_1K_100b.dat", nbins=100)
 
 # Fijando r3 = 23 MPc -- index = 4
 fixed_value_r3 = xi[:,:,4] 
@@ -99,3 +106,4 @@ fig, ax = plt.subplots()
 plt.imshow(fixed_value_r3, origin="lower",cmap=RdBu, interpolation="bilinear",norm=norm, vmin=-1,vmax=1)
 plt.colorbar()
 plt.show()
+"""
