d276e98968046de5eb866e0a863f2a877c5373d9
AlejandroGoper <ia.gomezperez@ugto.mx>
2021-12-16 14:32:49 -0600

    Modificacion caja 1GP

--- Simulaciones_de_cajas/histograms/2PCF/promediador2PCF.py
+++ Simulaciones_de_cajas/histograms/2PCF/promediador2PCF.py
@@ -17,9 +17,9 @@ import matplotlib.pyplot as plt
 from PCF import PCF,get_histogram
 
 
-dd,e_dd = get_histogram("DD", "512MPc", 15)
-dr,e_dr = get_histogram("DR", "512MPc", 15)
-rr,e_rr = get_histogram("RR", "512MPc", 15)
+dd,e_dd = get_histogram("DD", "1GPc", 10)
+dr,e_dr = get_histogram("DR", "1GPc", 10)
+rr,e_rr = get_histogram("RR", "1GPc", 10)
 
 bins = np.arange(30)
 bins += 1
@@ -35,14 +35,16 @@ error = _2PCF.error_estimador(E_DD=e_dd, E_DR=e_dr, E_RR=e_rr)
 # Graficando histograma 
 
 plt.figure(figsize=(20,10))
-plt.errorbar(bins,est*bins*bins,yerr = error*(bins*bins), ecolor="black" ,elinewidth = 3, capsize = 10 ,
-             color = "lightgray",fmt = '-o',mfc="red", ms = 10, label="Box: 512 MPc")
+plt.errorbar(bins,est*bins*bins,yerr = error*(bins*bins), ecolor="red",
+             linestyle="--" , elinewidth = 1, capsize = 5 , 
+             color = "gray",fmt = '-o', 
+             mfc="black", ms = 10, label="Box: 1 GPc")
 plt.legend(fontsize=15,loc="upper left")
 plt.grid()
-plt.title("2PCF - Dmax: 140 - Bins: 30",fontsize=22)
+plt.title(r"The 2PCF: $d_{max} = 140$,  Bins = 30",fontsize=22)
 plt.xlabel("$r$ [$MPc$]",fontsize=18)
 plt.xticks(fontsize=15)
 plt.ylabel("$\epsilon_{s-szalay} r^{2}$",fontsize=22)
 plt.yticks(fontsize=15)
-plt.savefig("2PCF_512MPc.png")
+plt.savefig("2PCF_1GPc.png")
 plt.show()
